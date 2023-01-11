0af269a41968a3b8f23eac9d6c331ce993202134
Freddy Heppell <freddy@freddyheppell.com>
2023-01-11 10:29:38 +0000

    Add skipped tests for future unusual link resolution

--- tests/extractors/data/test_link_resolver.py
+++ tests/extractors/data/test_link_resolver.py
@@ -1,3 +1,5 @@
+import pytest
+
 from extractor.extractors.data.link_resolver import resolve_link
 from extractor.extractors.data.links import LinkRegistry, ResolvableLink
 
@@ -34,3 +36,27 @@ def test_link_resolver_preview_page():
         None,
     )
     assert resolve_link(registry, resolvable).destination == registry.links[0]
+
+
+@pytest.mark.skip(reason="TODO: allow relative links")
+def test_link_resolver_relative():
+    registry = LinkRegistry()
+    registry.add_linkable("https://example.org/post1", "post", 1)
+
+    resolvable = ResolvableLink("test", "/post1", None)
+
+    assert resolve_link(registry, resolvable).destination == registry.links[0]
+
+
+@pytest.mark.skip(reason="TODO: trailing slash resolution")
+def test_link_resolver_trailing_slash():
+    registry = LinkRegistry()
+    registry.add_linkable("https://example.org/with/", "post", 1)
+    registry.add_linkable("https://example.org/without", "post", 2)
+
+    resolvable_1 = ResolvableLink("test", "https://example.org/with", None)
+
+    resolvable_2 = ResolvableLink("test", "https://example.org/without/", None)
+
+    assert resolve_link(registry, resolvable_1).destination == registry.links[0]
+    assert resolve_link(registry, resolvable_2).destination == registry.links[1]
