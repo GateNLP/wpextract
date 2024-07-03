cf95bd69476a36464ef80ce6517500961e598e19
Freddy Heppell <freddy@freddyheppell.com>
2024-07-03 14:57:59 +0100

    remove object display code

--- src/extractor/dl/downloader.py
+++ src/extractor/dl/downloader.py
@@ -52,44 +52,24 @@ class WPDownloader:
             obj_type: the type of the object
             plural: whether the name must be plural or not
         """
-        display_func = None
         export_func = None
-        additional_info = {}
         obj_name = ""
         if obj_type == WPApi.USER:
-            display_func = InfoDisplayer.display_users
             export_func = Exporter.export_users
-            additional_info = {}
             obj_name = "Users" if plural else "User"
         elif obj_type == WPApi.TAG:
-            display_func = InfoDisplayer.display_tags
             export_func = Exporter.export_tags
-            additional_info = {}
             obj_name = "Tags" if plural else "Tag"
         elif obj_type == WPApi.CATEGORY:
-            display_func = InfoDisplayer.display_categories
             export_func = Exporter.export_categories
-            additional_info = {"category_list": self.scanner.categories}
             obj_name = "Categories" if plural else "Category"
         elif obj_type == WPApi.POST:
-            display_func = InfoDisplayer.display_posts
             export_func = Exporter.export_posts
-            additional_info = {
-                "tags_list": self.scanner.tags,
-                "categories_list": self.scanner.categories,
-                "users_list": self.scanner.users,
-            }
             obj_name = "Posts" if plural else "Post"
         elif obj_type == WPApi.PAGE:
-            display_func = InfoDisplayer.display_pages
             export_func = Exporter.export_pages
-            additional_info = {
-                "parent_pages": self.scanner.pages,
-                "users": self.scanner.users,
-            }
             obj_name = "Pages" if plural else "Page"
         elif obj_type == WPApi.COMMENT:
-            display_func = InfoDisplayer.display_comments
             export_func = Exporter.export_comments_interactive
             additional_info = {
                 #'parent_posts': self.scanner.posts, # May be too verbose
@@ -97,24 +77,18 @@ class WPDownloader:
             }
             obj_name = "Comments" if plural else "Comment"
         elif obj_type == WPApi.MEDIA:
-            display_func = InfoDisplayer.display_media
             export_func = Exporter.export_media
-            additional_info = {"users": self.scanner.users}
             obj_name = "Media"
         elif obj_type == WPApi.NAMESPACE:
-            display_func = InfoDisplayer.display_namespaces
             export_func = Exporter.export_namespaces
-            additional_info = {}
             obj_name = "Namespaces" if plural else "Namespace"
 
         return {
-            "display_func": display_func,
             "export_func": export_func,
-            "additional_info": additional_info,
             "obj_name": obj_name,
         }
 
-    def _list_obj(self, obj_type, start=None, limit=None, is_all=True, cache=True):
+    def _list_obj(self, obj_type, start=None, limit=None, cache=True):
         prop = self._get_fetch_or_list_type(obj_type, plural=True)
         print(prop["obj_name"] + " details")
 
@@ -125,13 +99,11 @@ class WPDownloader:
             obj_list = self.scanner.get_obj_list(
                 obj_type, start, limit, cache, kwargs=kwargs
             )
-            # prop["display_func"](obj_list)
-            InteractiveShell.export_decorator(
-                prop["export_func"],
-                is_all,
-                prop["obj_name"].lower(),
-                self.out_path,
-                csv=None,
+
+            WPDownloader.export_decorator(
+                export_func=prop["export_func"],
+                export_str=prop["obj_name"].lower(),
+                json=self.out_path,
                 values=obj_list,
             )
         except WordPressApiNotV2:
@@ -139,3 +111,12 @@ class WPDownloader:
         except IOError as e:
             Console.log_error("Could not open %s for writing" % e.filename)
         print()
+
+    @staticmethod
+    def export_decorator(  # noqa: D102
+        export_func, export_str, json, values, kwargs=None
+    ):
+        kwargs = kwargs or {}
+        if json is not None:
+            json_file = json + "-" + export_str
+            export_func(values, Exporter.JSON, json_file, **kwargs)
