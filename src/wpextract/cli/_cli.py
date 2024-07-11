03ade1ba9a145848cc7c9ef8a46fbba12fb389c7
Freddy Heppell <freddy@freddyheppell.com>
2024-07-11 15:56:16 +0100

    Ruff

--- src/wpextract/cli/_cli.py
+++ src/wpextract/cli/_cli.py
@@ -12,10 +12,10 @@ PYTHON_VERSION = platform.python_version()
 @click.group(context_settings=dict(help_option_names=["-h", "--help"]), **CMD_ARGS)
 @click.version_option(
     package_name="wpextract",
-    message=f"%(prog)s, version %(version)s (Python {PYTHON_VERSION})"
+    message=f"%(prog)s, version %(version)s (Python {PYTHON_VERSION})",
 )
 def cli():
-    """Wpextract is a tool to create datasets from WordPress sites."""
+    """WPextract is a tool to create datasets from WordPress sites."""
 
 
 cli.add_command(download)
