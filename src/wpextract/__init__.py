184183eb901723f6c17cec0bc61fc4a3399a9d65
Freddy Heppell <freddy@freddyheppell.com>
2024-07-15 15:00:01 +0100

    Add package version attribute (#36)
    * Add version attribute to package
    
    * Revert "Hotfix: remove usage of __version__ in docs (#35)"
    
    This reverts commit 641375a9bafb6e1feccbd59ae257846b5ee5c38c.

--- src/wpextract/__init__.py
+++ src/wpextract/__init__.py
@@ -1,3 +1,7 @@
+from importlib.metadata import version
+
 from wpextract.downloader import WPDownloader as WPDownloader
 
 from .extract import WPExtractor as WPExtractor
+
+__version__ = version("wpextract")
